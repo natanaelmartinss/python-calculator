91e6c01db069cc54ac0240534027cac019fbe116
natanaelmartinss <natanaelmartins303@gmail.com>
2026-09-17 19:32:22 -0300

    Calculator updated with all commands in English.

---
+++ calculator.py
@@ -0,0 +1,73 @@
+
+def interface():
+    print("=========Calculadora==========")
+    print("1. Somar(+)")
+    print("2. Subtrair(-)")
+    print("3. Multiplicar(*)")
+    print("4. Dividir(÷)")
+    print("5. Potenciação(xʸ)")
+    print("6. Raiz quadrada(√)")
+    print("7. Sair")
+
+def info_receive():
+    interface()
+    operator = input("Selecione qual tipo de operação deseja realizar:")
+    if operator not in ["1", "2", "3", "4", "5", "6", "7"]:
+        print("Opção Invalida! Selecione uma válida:")
+        return info_receive()
+    
+    if operator == "7":
+        return operator, None, None
+
+    try:
+        a = float(input("Primeiro número da operação:"))
+        if operator == "6":
+            return operator, a, None
+    except ValueError:
+        print("Digite apenas números!")
+        return info_receive()
+
+    try:
+        b = float(input("Segundo número da operação:"))
+        if operator == "4":
+            if b == 0:
+                print("Impossível dividir qualquer número por zero!")
+                return info_receive()
+        return operator, a, b
+    except ValueError:
+        print("Digite apenas números!")
+        return info_receive()
+
+
+def operation(operator, a, b):
+    match operator:
+
+        case "1":
+            result = a + b
+            print(f"O resultado da sua operação é {result:.3f}!")
+            
+        case "2":
+            result = a - b
+            print(f"O resultado da sua operação é {result:.3f}!")
+
+        case "3":
+            result = a * b
+            print(f"O resultado da sua operação é {result:.3f}!")
+
+        case "4":
+            result = a / b  
+            print(f"O resultado da sua operação é {result:.3f}!")
+
+        case "5":
+            result = a ** b  
+            print(f"O resultado da sua operação é {result:.3f}!")
+
+        case "6":
+            result = a ** 0.5 
+            print(f"O resultado da sua operação é {result:.3f}!")
+
+        case "7":
+            print("Obrigado por utilizar!")
+
+        case _:
+            print("Opção inválida!")
